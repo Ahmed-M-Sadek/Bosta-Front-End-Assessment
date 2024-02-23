f902bf6b8d576f89da0a9c2dd016b10e14ecfddd
Ahmed-M-Sadek <ahmedms.abdelhamid@gmail.com>
2024-02-23 05:01:37 +0200

    adjust tracking no. search bar
    removed unnecessary components and files that do not fit the requirements
    installed more required packages
    added icons
    created a sticky footer

--- src/components/LanguageChanger.tsx
+++ src/components/LanguageChanger.tsx
@@ -1,21 +1,34 @@
-import { Button } from "antd";
+import { Space, Dropdown } from "antd";
+import type { MenuProps } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 
 function LanguageChanger() {
   const [t, i18n] = useTranslation("global");
-
-  const handleLanguageChange = (lang: string) => {
-    i18n.changeLanguage(lang);
+  const items: MenuProps["items"] = [
+    {
+      key: "en",
+      label: t("navbar.languageDropdownEnglish"),
+    },
+    {
+      key: "ar",
+      label: t("navbar.languageDropdownArabic"),
+    },
+  ];
+  const onClick: MenuProps["onClick"] = ({ key }) => {
+    i18n.changeLanguage(key);
   };
 
   return (
     <>
-      <Button onClick={() => handleLanguageChange("en")}>
-        {t("navbar.languageDropdownEnglish")}
-      </Button>
-      <Button onClick={() => handleLanguageChange("ar")}>
-        {t("navbar.languageDropdownArabic")}
-      </Button>
+      <Dropdown menu={{ items, onClick }}>
+        <a onClick={(e) => e.preventDefault()}>
+          <Space>
+            {t("navbar.currentLanguage")}
+            <DownOutlined />
+          </Space>
+        </a>
+      </Dropdown>
     </>
   );
 }
